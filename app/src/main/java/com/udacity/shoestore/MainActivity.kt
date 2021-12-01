95d42889706d8366651d96dcfa3e69714021f28b
Molikkidd <mzkidd5@gmail.com>
2021-12-01 14:10:57 -0800

    update: navigation and appbar

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,6 +2,8 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
@@ -10,27 +12,26 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 //    connect drawer to mainactivity
-    private lateinit var drawerLayout: DrawerLayout
+
     private lateinit var appBarConfiguration: AppBarConfiguration
 
 //    create main page activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-//    add side menu when page is being rendered
-        drawerLayout = binding.drawerLayout
+
 //connect to navcontroller on act_main.xml
     val navController = findNavController(R.id.myNavHostFragment)
-//    connect Navi UI to Navi Controller
-    NavigationUI.setupWithNavController(binding.navView, navController)
     appBarConfiguration = AppBarConfiguration(navController.graph)
-    drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
-    onSupportNavigateUp()
 
+    //    connect Navi UI to Navi Controller
+    NavigationUI.setupActionBarWithNavController(this, navController , appBarConfiguration)
     }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
         return NavigationUI.navigateUp(navController, appBarConfiguration)
